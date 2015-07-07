c510b716cad2f15d9a6eef60e843d164ab987aec
Constanti <constantin.dumba@gmail.com>
2015-07-07 10:22:23 -1000

    changed variable names

--- smallest_multiple.js
+++ smallest_multiple.js
@@ -5,23 +5,22 @@
  *  @returns the smallest positive number that is evenly divisible by all numbers
  */
 module.exports = function(numbers){
-  var multiple = numbers;
+  var smallestMultiple = numbers;
   var highestFactor = numbers;
   // var startNumber = highestFactor * (highestFactor -1) ;
-  while(multiple < 100000000000000){
+  while(smallestMultiple < Infinity){
     var n = numbers;
     while(n > 1){
       var found = true;
-      if(multiple%n !== 0){
+      if(smallestMultiple%n !== 0){
         found = false;
         break;
       }
       n--;
     }
     if(found){
-      return multiple;
+      return smallestMultiple;
     }
-    multiple += highestFactor;
-
+    smallestMultiple += highestFactor;
   }
 };
